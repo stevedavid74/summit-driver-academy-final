b3765354a59c7e9b16f7f72b91ce9f450acd0794
stevedavid74 <dstephane067@gmail.com>
2026-08-14 06:51:10 -0400

    add secure portal configuration

---
+++ portal/portal-config.js
@@ -0,0 +1,4 @@
+window.SUMMIT_PORTAL_CONFIG = Object.freeze({
+  supabaseUrl: 'https://zrnylkqtofjitxtykhxq.supabase.co',
+  supabasePublishableKey: 'sb_publishable_x1l4_lhViir_VF7HovlPeA_TyQemlmH'
+});
